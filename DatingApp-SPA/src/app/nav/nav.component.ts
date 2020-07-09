cda3892bd6fb9a6e82e5e092266ea12f3ac8422f
Iliayn Hristov <iliqn408@abv.bg>
2020-07-09 13:04:22 +0300

    Add Register and Login UI elements.

---
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { AuthService } from '../_services/auth.service';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.css']
+})
+export class NavComponent implements OnInit {
+  model: any = {};
+
+  constructor(private authService: AuthService) { }
+
+  ngOnInit() {
+  }
+
+  login() {
+    this.authService.login(this.model).subscribe(next => {
+      console.log('Logged in successfuly!');
+    }, error => {
+      console.log(error);
+    });
+  }
+
+  loggedIn() {
+    const token = localStorage.getItem('token');
+    return !!token;
+  }
+
+  loggOut() {
+    localStorage.removeItem('token');
+    console.log('logged out');
+  }
+}
